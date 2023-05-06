5a8ac7ea7f403efee700cd3cb3d4e2c0fe2e70d4
Hanna Vakulenko <anna.vakulenko.ua@gmail.com>
2023-05-06 13:34:49 +0300

    add Suspense for code splitting, searchbox and location

---
+++ src/pages/Login.jsx
@@ -0,0 +1,45 @@
+// the first way:
+
+// import { useNavigate } from 'react-router-dom';
+
+// export const Login = () => {
+//   const navigate = useNavigate();
+
+//   const handleSubmit = async values => {
+//     const response = await FakeAPI.login(values);
+//     if (response.success) {
+//       navigate('/profile', { replace: true });
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h1>Login page</h1>
+//       <LoginForm onSubmit={handleSubmit} />
+//     </div>
+//   );
+// };
+
+// the second way:
+
+// import { Navigate, useState } from 'react-router-dom';
+
+// export const Login = () => {
+//   const [isLoginSuccess, setIsLoginSuccess] = useState(false);
+
+//   const handleSubmit = async values => {
+//     const response = await FakeAPI.login(values);
+//     setIsLoginSuccess(response.success);
+//   };
+
+//   if (isLoginSuccess) {
+//     return <Navigate to="/profile" replace />;
+//   }
+
+//   return (
+//     <div>
+//       <h1>Login page</h1>
+//       <LoginForm onSubmit={handleSubmit} />
+//     </div>
+//   );
+// };
